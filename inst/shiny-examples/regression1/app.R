8e615513726af8c5bc8bd9267b4fa8c35bb4cc7b
Charles Zheng <snarles@stanford.edu>
2016-06-07 19:02:59 -0700

    about to change formatting

--- inst/shiny-examples/regression1/app.R
+++ inst/shiny-examples/regression1/app.R
@@ -1,6 +1,8 @@
 pts <- rbind(c(1,1), c(2,2), c(3,3))
 sel_pt <- 0
 DIST_THRES <- 0.1
+MIN_X <- -2
+MAX_X <- 2
 
 shinyApp(
   ui = fluidPage(
@@ -30,9 +32,12 @@ shinyApp(
       column(width = 3,
              div(class = "option-group",
                  div(class = "option-header", "Generation"),
-                 numericInput("par_n", "N: ", 20),
+                 numericInput("par_n", "N: ", 10),
                  selectInput("par_d", "True degree: ", choices = 0:3,
                             selected = 1),
+                 selectInput("par_distro", "Noise type: ", choices = c("Normal", "Laplacian", "Cauchy"),
+                             selected = "Normal"),
+                 numericInput("par_sigma", "noise:", 1),
                  numericInput("par_seed", "seed: ", 0)
              )),
       column(width = 3,
@@ -43,20 +48,31 @@ shinyApp(
                  selectInput("fit_d", "Objective: ",
                              choices = c("Absolute", "Squared"),
                              selected = "Squared"),
-                 radioButtons("fit_hide", "Hide SSE?:",
-                              choices = c("Hide", "Show"),
-                              selected = "Hide")
+                 checkboxInput("fit_sse", "Show objective", FALSE),
+                 checkboxInput("fit_opt", "Show optimal fit", FALSE)
              ))
     ),
     fluidRow(
-      column(width = 4,
+      column(width = 6,
              uiOutput("plotUI1"))
     )
   ),
-  
+
   server = function(input, output, session) {
     curData <- reactive({
+      d <- as.numeric(input$par_d)
       set.seed(input$par_seed)
+      bt <- rnorm(1 + d)
+      n <- input$par_n
+      x1 <- runif(input$par_n) * (MAX_X - MIN_X) + MIN_X
+      xs <- do.call(cbind, lapply(0:d, function(i) x1^i))
+      noises <- input$par_sigma * switch (input$par_distro,
+        "Normal" = rnorm(n),
+        "Laplacian" = rexp(n) * (2 * rbinom(n) - 1),
+        "Cauchy" = rcauchy(n)
+      )
+      ys <- xs %*% bt + noises
+      data.frame(x = x1, y = ys)
     })
     curPoints <- reactive({
       npts <- as.numeric(input$fit_d) + 1
@@ -74,14 +90,14 @@ shinyApp(
       pts[1:npts, ]
     })
     output$plotUI1 <- renderUI({
-      plotOutput("plot1", height = 300,
+      plotOutput("plot1", height = 500,
                  click = "plot1_click")
     })
     output$plot1 <- renderPlot({
-      plot(curPoints(), xlab = "x", ylab = "y", xlim = c(0, 4), ylim = c(0, 4))
-      if (sel_pt != 0) {
-        points(curPoints()[sel_pt, , drop=FALSE], cex = 3, col = "red")
-      }
+      plot(curData(), xlab = "x", ylab = "y", xlim = c(MIN_X, MAX_X))
+      # if (sel_pt != 0) {
+      #   points(curPoints()[sel_pt, , drop=FALSE], cex = 3, col = "red")
+      # }
     })
   }
 )
