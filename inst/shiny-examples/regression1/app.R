d0b0c25a83486cc71da15754733fdcd4d13f3448
Charles Zheng <snarles@stanford.edu>
2016-06-07 19:58:00 -0700

    manual selection and display

--- inst/shiny-examples/regression1/app.R
+++ inst/shiny-examples/regression1/app.R
@@ -1,8 +1,22 @@
-pts <- rbind(c(1,1), c(2,2), c(3,3))
 sel_pt <- 0
 DIST_THRES <- 0.1
 MIN_X <- -2
 MAX_X <- 2
+RESO <- 10
+RESO2 <- 100
+pts <- cbind(c(1/6, 5/6, 3/6, 2/6, 4/6) * (MAX_X - MIN_X) + MIN_X, 0)
+
+xterms <- c("", " * x", " * x^2", " * x^3", " * x^4", " * x^4")
+eqstr <- function(bt) {
+  bt <- floor(bt * RESO2)/RESO2
+  paste("y = ", paste(paste(bt, xterms[1:length(bt)], sep = ""), collapse = " + "))
+}
+
+bth <- function(pts, dfit) {
+  px <- do.call(cbind, lapply(0:dfit, function(i) pts[, 1]^i))
+  pbt <- MASS::ginv(px) %*% pts[, 2]
+  pbt
+}
 
 shinyApp(
   ui = fluidPage(
@@ -25,12 +39,16 @@ shinyApp(
                    selectInput("fit_d", "Objective: ",
                                choices = c("Absolute", "Squared"),
                                selected = "Squared"),
+                   checkboxInput("fit_pts", "Manually select points", FALSE),
                    checkboxInput("fit_sse", "Show objective", FALSE),
-                   checkboxInput("fit_opt", "Show optimal fit", FALSE)
-                   ))
+                   checkboxInput("fit_opt", "Show optimal fit", FALSE),
+                   DT::dataTableOutput('mytable')
+                   )),
+        width = 6
       ),
       mainPanel(
-        uiOutput("plotUI1")
+        uiOutput("plotUI1"),
+        width = 4
       )
     )
   ),
@@ -45,13 +63,29 @@ shinyApp(
       xs <- do.call(cbind, lapply(0:d, function(i) x1^i))
       noises <- input$par_sigma * switch (input$par_distro,
         "Normal" = rnorm(n),
-        "Laplacian" = rexp(n) * (2 * rbinom(n) - 1),
+        "Laplacian" = rexp(n) * (2 * rbinom(n, 1, 0.5) - 1),
         "Cauchy" = rcauchy(n)
       )
       ys <- xs %*% bt + noises
-      data.frame(x = x1, y = ys)
+      x1 <- floor(x1 * RESO)/RESO
+      ys <- ys - 0.7 * mean(ys)
+      ys <- floor(ys * RESO)/RESO
+      o <- order(x1)
+      data.frame(x = x1[o], y = ys[o])
+    })
+    curTable <- reactive({
+      tab <- curData()
+      dfit <- as.numeric(input$fit_d)
+      if (input$fit_pts) {
+        ptz <- curPoints()
+        bth <- bth(ptz, dfit)
+        xs <- do.call(cbind, lapply(0:dfit, function(i) tab$x^i))
+        tab$fit.y <- floor(as.numeric(xs %*% bth) * RESO2)/RESO2
+      }
+      tab
     })
     curPoints <- reactive({
+      dfit <- as.numeric(input$fit_d)
       npts <- as.numeric(input$fit_d) + 1
       if (!is.null(input$plot1_click)) {
         p1c <- c(input$plot1_click$x, input$plot1_click$y)
@@ -64,17 +98,39 @@ shinyApp(
           sel_pt <<- which.min(dd)
         }
       }
-      pts[1:npts, ]
+      pts[1:npts, , drop = FALSE]
     })
     output$plotUI1 <- renderUI({
-      plotOutput("plot1", height = 500,
+      plotOutput("plot1", height = 400,
                  click = "plot1_click")
     })
     output$plot1 <- renderPlot({
-      plot(curData(), xlab = "x", ylab = "y", xlim = c(MIN_X, MAX_X))
-      # if (sel_pt != 0) {
-      #   points(curPoints()[sel_pt, , drop=FALSE], cex = 3, col = "red")
-      # }
+      dat <- curData()
+      plot(dat, xlab = "x", ylab = "y", xlim = c(MIN_X, MAX_X))
+      d <- as.numeric(input$par_d)
+      dfit <- as.numeric(input$fit_d)
+      if (input$fit_pts) {
+        ptz <- curPoints()
+        for (i in 1:(dfit + 1)) {
+          cc <- "blue"; cx <- 1
+          if (i == sel_pt) {
+            cc <- "red"
+            cx <- 1.5
+          }
+          points(ptz[i, , drop = FALSE], pch = 9, col = cc, cex = cx)
+        }
+      }
+      if (input$fit_pts) {
+        ptz <- curPoints()
+        sx <- seq(MIN_X, MAX_X, length.out = 100)
+        sX <- do.call(cbind, lapply(0:dfit, function(i) sx^i))
+        sy <- as.numeric(sX %*% bth(ptz, dfit))
+        lines(sx, sy, col = "blue")
+        title(eqstr(bth(ptz, dfit)))
+      }
+    })
+    output$mytable <- DT::renderDataTable({
+      DT::datatable(curTable())
     })
   }
 )
