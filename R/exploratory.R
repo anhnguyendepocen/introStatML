256749ebab7e51d415a6b453e7f26843c4299f60
Charles Zheng <snarles@stanford.edu>
2016-06-13 18:15:48 -0700

    roll call data function

--- R/exploratory.R
+++ R/exploratory.R
@@ -8,3 +8,15 @@ sample_rows <- function(tab, k = 5, replace = FALSE) {
   tab[sample(nrow(tab), k, replace), ]
 }
 
+#' Get data from 2014 house of representatives and print metadata
+#'
+#' @param rolls Which rolls to obtain, 1-99
+#' @param print Whether to print metadata
+#' @return A dataframe
+#' @export
+roll_data <- function(rolls = 1:99, print = TRUE) {
+  if (print) {
+    print(roll2014meta[rolls, -5])
+  }
+  roll2014votes[, c(2:3, rolls + 3)]
+}
