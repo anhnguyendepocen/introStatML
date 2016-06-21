8380e4a9a9362608a5d94c1c31972c1526865028
Charles Zheng <snarles@stanford.edu>
2016-06-21 05:33:06 -0700

    lecture 2 stuff

--- R/blackbox1.R
+++ R/blackbox1.R
@@ -8,6 +8,21 @@ blackbox01_hidden <- function(n = 5) {
   x[1:n]
 }
 
+#' An unknown discrete distribution
+#'
+#' @param n Number of observations
+#' @return Numeric vector
+possibly_unfair_die1_hidden <- function(n = 5) {
+  sample(1:6, size = n, replace = TRUE, prob = c(0.1, 0.1, 0.2, 0.1, 0.2, 0.3))
+}
+
+#' An unknown discrete distribution
+#'
+#' @param n Number of observations
+#' @return Numeric vector
+possibly_unfair_die2_hidden <- function(n = 5) {
+  sample(1:6, size = n, replace = TRUE)
+}
 
 #' An unknown discrete distribution
 #'
@@ -17,3 +32,25 @@ blackbox01_hidden <- function(n = 5) {
 blackbox01 <- function(n = 5) {
   blackbox01_hidden(n)
 }
+
+
+
+
+#' An unknown discrete distribution
+#'
+#' @param n Number of observations
+#' @return Numeric vector
+#' @export
+possibly_unfair_die1 <- function(n = 5) {
+  possibly_unfair_die1_hidden(n)
+}
+
+#' An unknown discrete distribution
+#'
+#' @param n Number of observations
+#' @return Numeric vector
+#' @export
+possibly_unfair_die2 <- function(n = 5) {
+  possibly_unfair_die2_hidden(n)
+}
+
